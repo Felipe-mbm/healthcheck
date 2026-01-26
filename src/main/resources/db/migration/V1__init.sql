0daa2e119073e13b5bd006fc437bdea69c92ebe4
FelipeMessaMarques <felipembmarques@gmail.com>
2026-01-26 18:12:36 -0300

    fix: Corrects migration V1 to create the outages table along with the database creation.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,4 @@
--- V1: Migration para criar a primeira tabela no banco de dados
+-- V1: Migration to create the initial database tables
 
 CREATE TABLE users (
     id VARCHAR(36) PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -8,15 +8,13 @@ CREATE TABLE users (
     last_active_at TIMESTAMP
 );
 
-
-CREATE TABLE monitored_url (
-    id VARCHAR(36) PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL UNIQUE,
-    url VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_active BOOLEAN DEFAULT TRUE,
-    last_status VARCHAR(20),
-    last_checked_at TIMESTAMP
+CREATE TABLE outages (
+    id VARCHAR(36) PRIMARY KEY DEFAULT gen_random_uuid()::text,
+    url_id VARCHAR(36) NOT NULL,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    reason VARCHAR(255),
+    CONSTRAINT fk_outage_url FOREIGN KEY (url_id) REFERENCES monitored_urls(id) ON DELETE CASCADE
 );
 
 CREATE TABLE monitor_log (
