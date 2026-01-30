cee1fa540a9e8285760fc78449cb3420162bce30
FelipeMessaMarques <felipembmarques@gmail.com>
2026-01-30 14:35:48 -0300

    fix: upgrade spring boot 3.4.1 and fix flyway compatibility

--- src/main/java/com/example/health_check/model/entity/User.java
+++ src/main/java/com/example/health_check/model/entity/User.java
@@ -2,31 +2,81 @@ package com.example.health_check.model.entity;
 
 import com.example.health_check.model.enums.UserRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "role", nullable = false)
     public UserRole userRole = UserRole.USER;
 
     @Column(name = "email")
     private String email;
 
-    @Column(name = "checked_interval")
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "check_interval")
     private Integer checkInterval = 1;
 
     @Column(name = "last_active_at")
     private LocalDateTime lastActiveAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.userRole == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
